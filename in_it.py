eaebe2c97c1dc90ee21eaeb98fba0801fbd88b36
winsem <48600217+winsem@users.noreply.github.com>
2021-04-11 14:46:23 +0530

    Create in_it.py

---
+++ in_it.py
@@ -0,0 +1,4 @@
+import os
+os.system('pip3 install twint')
+os.system('pip3 install --user --upgrade git+https://github.com/twintproject/twint.git@origin/master#egg=twint')
+print('Twint is installed')
